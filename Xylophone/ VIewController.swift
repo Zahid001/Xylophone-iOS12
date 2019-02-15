b3c331b08cde02f7b10fb760a91748af68d5eddf
Zahid001 <islam35-1483@diu.edu.bd>
2019-02-15 20:58:49 +0600

    keyboard tone

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController{
     
 
+    var audioPlayer = AVAudioPlayer()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,9 +22,45 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            playTone(trackName: "note1", ext: "wav")
+            break
+        case 2:
+            playTone(trackName: "note2", ext: "wav")
+            break
+        case 3:
+            playTone(trackName: "note3", ext: "wav")
+            break
+        case 4:
+            playTone(trackName: "note4", ext: "wav")
+            break
+        case 5:
+            playTone(trackName: "note5", ext: "wav")
+            break
+        case 6:
+            playTone(trackName: "note6", ext: "wav")
+            break
+        case 7:
+            playTone(trackName: "note7", ext: "wav")
+            break
+        default:
+            print("invalid")
+        }
+        
+    }
+    
+    func playTone(trackName : String,ext:String)  {
+        let audioUrl = Bundle.main.url(forResource: trackName, withExtension: ext)
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl!)
+        }
+        catch{
+            print(error)
+        }
         
         
-        
+        audioPlayer.play()
     }
     
   
